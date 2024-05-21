f2a42cfb2066a9c9359991cebfbe4066d1c20fe5
safkat07 <safkatsadli@gmail.com>
2024-05-21 12:24:36 +0600

    clearance section complete

--- src/Components/ClearenceSell/ClearenceSell.jsx
+++ src/Components/ClearenceSell/ClearenceSell.jsx
@@ -1,16 +1,28 @@
+import Button from '../../UseAbleComponents/Button'
 import bgimg from '../../assets/kitchen-tools.jpg'
 
 const ClearenceSell = () => {
     return (
         <div className='h-screen relative bg-secondary'>
             <div
-                className="absolute bottom-0 bg-fixed w-full h-3/4 bg-cover bg-center z-[90]"
+                className="absolute bottom-0 bg-fixed w-full h-3/4 bg-cover bg-center z-[99]"
                 style={{ backgroundImage: `url(${bgimg})` }}
             >
-                <p className='text-3xl  text-primary z-[99]'>
-                    Save up-to 70% off!!
-                </p>
+                <div className='absolute inset-0 top-[40%]'>
+                    <p className='text-6xl text-center font-primary   text-primary '>
+                        Save up-to 70% off!!
+                    </p>
+                    <p className='text-8xl text-center font-primary   text-primary '>
+                        CLEARANCE SELL 2024
+                    </p>
+
+                    <div className="flex justify-center pt-16 items-center">
+                        <Button btnTitle={"Explore Now"} />
+                    </div>
+                </div>
+
             </div>
+
         </div>
     )
 }
