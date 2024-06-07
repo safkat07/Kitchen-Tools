54c6e6a8f8f07a4c8e11903976da10247165ed17
safkat07 <safkatsadli@gmail.com>
2024-06-07 23:31:38 +0600

    firebase added for hosting

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCYoHqCxWtSOFvy48l01mco3572ZwmnxAI",
+  authDomain: "thekitchen-778bb.firebaseapp.com",
+  projectId: "thekitchen-778bb",
+  storageBucket: "thekitchen-778bb.appspot.com",
+  messagingSenderId: "484957048904",
+  appId: "1:484957048904:web:e48af63c3246880f15c87d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
